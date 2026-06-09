13928f7e88322b2840d6d6e8ba672e2ed297e2be
jassuwu <jass@jass.gg>
2026-06-09 17:35:50 +0530

    feat(remotion): register hero + social compositions

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -1,17 +1,28 @@
 import "./index.css";
 import { Composition } from "remotion";
-import { MyComposition } from "./Composition";
+import { HERO_DURATION, SOCIAL_DURATION } from "./choreography";
+import { QuiltMerge } from "./QuiltMerge";
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
       <Composition
-        id="MyComp"
-        component={MyComposition}
-        durationInFrames={60}
+        id="hero"
+        component={QuiltMerge}
+        durationInFrames={HERO_DURATION}
         fps={30}
-        width={1280}
-        height={720}
+        width={1920}
+        height={1080}
+        defaultProps={{ showCta: false }}
+      />
+      <Composition
+        id="social"
+        component={QuiltMerge}
+        durationInFrames={SOCIAL_DURATION}
+        fps={30}
+        width={1920}
+        height={1080}
+        defaultProps={{ showCta: true }}
       />
     </>
   );
